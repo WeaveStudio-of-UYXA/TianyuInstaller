75c6ce26610e6e1bdb6a71109e9271215b229543
Yayin Tsing <bingshihuakong@163.com>
2022-08-24 00:32:10 +0800

    搭建基本框架

---
+++ TianyuInstaller/Base/AndroidAPI.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Macro.h"
+
+#if DEPLOT == 2
+namespace TianyuAPI
+{
+	class Android :public QObject
+	{
+		Q_OBJECT
+		Public static bool requestStoragePermission() {
+			QtAndroid::PermissionResult r = QtAndroid::checkPermission("android.permission.WRITE_EXTERNAL_STORAGE");
+			if (r == QtAndroid::PermissionResult::Denied) {
+				QtAndroid::requestPermissionsSync(QStringList() << "android.permission.WRITE_EXTERNAL_STORAGE");
+				r = QtAndroid::checkPermission("android.permission.WRITE_EXTERNAL_STORAGE");
+				if (r == QtAndroid::PermissionResult::Denied) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+	};
+}
+#endif
